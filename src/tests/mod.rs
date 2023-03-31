90ef35b9ad68bbe6fc1ad39ae764d2875708f546
James Hodgkinson <james@terminaloutcomes.com>
2023-04-01 07:53:13 +1000

    more yak shaving

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -32,26 +32,18 @@ pub fn get_serverconfig(configtype: TestServerConfig) -> Result<ServerConfig, St
         Err(_) => 8089.to_string(),
     };
     let port: u16 = port.parse::<u16>().unwrap();
-    match configtype {
-        TestServerConfig::Hec => Ok(ServerConfig {
-            hostname,
-            port,
-            auth_method: crate::search::AuthenticationMethod::Token {
-                token: env::var(format!("{env_prefix}TOKEN"))
-                    .expect("Couldn't get SPLUNK_HEC_TOKEN env var"),
-            },
-            ..Default::default()
-        }),
-        TestServerConfig::Api => Ok(ServerConfig {
-            hostname,
-            port,
-            auth_method: crate::search::AuthenticationMethod::Basic {
-                username: env::var("SPLUNK_USERNAME")
-                    .expect("Couldn't get SPLUNK_USERNAME env var!"),
-                password: env::var("SPLUNK_PASSWORD")
-                    .expect("Couldn't get SPLUNK_PASSWORD env var!"),
-            },
-            ..Default::default()
-        }),
-    }
+
+    let config = ServerConfig::new(hostname).with_port(port);
+    let config = match configtype {
+        TestServerConfig::Hec => {
+            let token = env::var(format!("{env_prefix}TOKEN"))
+                .expect("Couldn't get SPLUNK_HEC_TOKEN env var");
+            config.with_token(token)
+        }
+        TestServerConfig::Api => config.with_username_password(
+            env::var("SPLUNK_USERNAME").expect("Couldn't get SPLUNK_USERNAME env var!"),
+            env::var("SPLUNK_PASSWORD").expect("Couldn't get SPLUNK_PASSWORD env var!"),
+        ),
+    };
+    Ok(config)
 }
