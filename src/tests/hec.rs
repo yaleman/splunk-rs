954b6a39f0c73cfba70ada2f846567b58219e205
James Hodgkinson <james@terminaloutcomes.com>
2023-04-01 11:16:19 +1000

    fixing features and testing for "hey send a batch of 1000  when we have 3"

--- src/tests/hec.rs
+++ src/tests/hec.rs
@@ -1,3 +1,7 @@
+use std::time::{UNIX_EPOCH, SystemTime};
+
+use serde_json::{Value, json};
+
 #[tokio::test]
 #[cfg_attr(feature = "test_ci", ignore)]
 async fn test_hec_endpoint_health() -> Result<(), String> {
@@ -43,4 +47,51 @@ async fn send_test_data() -> Result<(), String> {
     client
         .send_event(json!(format!("{{\"test\" : 1, \"_time\" : {unix_time}, \"message\" : \"Hello from splunk-rs testing\" }}")))
         .await.map_err(|e| e.to_string())
+}
+
+
+
+#[derive(serde::Serialize)]
+struct TestEvent {
+    test_name: String,
+    #[serde(alias="_time")]
+    time: u64,
+    message: String,
+}
+
+impl TestEvent {
+    #[allow(dead_code)]
+    fn new(test_name: impl ToString, message: impl ToString ) -> Self {
+
+        let now = SystemTime::now();
+        Self {
+            test_name: test_name.to_string(),
+            time: now.duration_since(UNIX_EPOCH).unwrap().as_secs(),
+            message: message.to_string()
+        }
+    }
+}
+
+impl From<TestEvent> for Value {
+    fn from(value: TestEvent) -> Self {
+        json!(value)
+    }
+}
+
+
+#[cfg_attr(feature = "test_ci", ignore)]
+#[tokio::test]
+async fn send_queued_multi_overized_batch() -> Result<(), String> {
+    use crate::hec::HecClient;
+
+    use crate::{ServerConfig, ServerConfigType};
+
+    let mut client = HecClient::with_serverconfig(ServerConfig::try_from_env(ServerConfigType::Hec)?);
+
+    for i in [0..3].iter() {
+        let event = TestEvent::new("send_queued_multi", format!("Event {:?}", i));
+        client.enqueue(event.into());
+    }
+    client.flush(Some(20)).await.map_err(|e| e.to_string())?;
+    Ok(())
 }
