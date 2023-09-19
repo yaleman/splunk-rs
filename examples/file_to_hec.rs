ab5abb3e2bfe548abc4506fbaed8a537c8cf9e81
James Hodgkinson <james@terminaloutcomes.com>
2023-09-19 19:17:20 +1000

    adding file_to_hec (#11)

---
+++ examples/file_to_hec.rs
@@ -0,0 +1,66 @@
+use std::io::Read;
+
+/// Pipe stdin to HTTP Event Collector!
+use clap::*;
+use serde_json::json;
+use splunk::hec::HecClient;
+
+#[derive(Parser)]
+struct Cli {
+    #[arg(short, long)]
+    index: Option<String>,
+    #[arg(short = 'n', long)]
+    hostname: Option<String>,
+    #[arg(short, long)]
+    port: Option<u16>,
+    #[arg(short, long)]
+    token: Option<String>,
+    #[arg(short, long)]
+    source: Option<String>,
+    #[arg(short = 'S', long)]
+    sourcetype: Option<String>,
+    filename: String,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), String> {
+    let cli = Cli::parse();
+
+    // in case they're using environment variables
+    let serverconfig = splunk::ServerConfig::try_from_env(splunk::ServerConfigType::Hec)?;
+
+    // set up the HecClient
+    let mut hec = HecClient::with_serverconfig(serverconfig);
+
+    if let Some(port) = cli.port {
+        hec.serverconfig = hec.serverconfig.with_port(port);
+    }
+    if let Some(hostname) = cli.hostname {
+        hec.serverconfig = hec.serverconfig.with_hostname(hostname);
+    }
+    if let Some(index) = cli.index {
+        hec = hec.with_index(index);
+    }
+    if let Some(val) = cli.source {
+        hec = hec.with_source(val)
+    } else {
+        hec = hec.with_source(&cli.filename);
+    };
+    if let Some(val) = cli.sourcetype {
+        hec = hec.with_sourcetype(val)
+    };
+
+    // open the file and read the contents into a buffer
+    let mut buffer = String::new();
+    let mut file = std::fs::File::open(cli.filename).map_err(|err| err.to_string())?;
+    file.read_to_string(&mut buffer)
+        .map_err(|err| err.to_string())?;
+
+    let data = json!(buffer.trim());
+    hec.enqueue(data).await;
+    match hec.flush(None).await {
+        Ok(val) => eprintln!("Sent {} events!", val),
+        Err(err) => eprintln!("Failure sending event: {err:?}"),
+    }
+    Ok(())
+}
